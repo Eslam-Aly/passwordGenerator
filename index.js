d3c8406eace8d5dac6c15b3f4c2563b6d2e5558e
Eslam-Aly <107219368+Eslam-Aly@users.noreply.github.com>
2022-10-26 20:55:18 +0200

    created needed function
    still need to fix the button

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
 "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`",
 "!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
 ",","|",":",";","<",">",".","?","/"];
+
 let passOneEl = document.getElementById("pass-one")
 let passTwoEl = document.getElementById("pass-two")
+let randomPassOne = ""
+let randomPassTwo = ""
 
 function getRandomPassOne() {
-    let randomPassOne = Math.floor(Math.random() * characters.length)
+    for (let i = 0; i < 16; i ++) {
+        randomPassOne +=  characters[Math.floor(Math.random() * characters.length)]
+    }
     return randomPassOne
 }
 function getRandomPassTwo() {
-    let randomPassTwo = Math.floor(Math.random() * characters.length)
-    return randomPassTow
-}
-
-
-function generatePassword() {
     for (let i = 0; i < 16; i ++) {
-        passTwoEl.textContent += randomPassOne
+        randomPassTwo +=  characters[Math.floor(Math.random() * characters.length)]
     }
-    return passTwoEl.textContent
+    return randomPassTwo
 }
+function generatePassword() {
+    passOneEl.textContent = getRandomPassOne()
+    passTwoEl.textContent = getRandomPassTwo()
+}
+
